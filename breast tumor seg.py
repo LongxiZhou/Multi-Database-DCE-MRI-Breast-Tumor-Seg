4a0646f221db718298ae935bfbc84219839ab022
LongxiZhou <50166538+LongxiZhou@users.noreply.github.com>
2021-08-25 17:59:22 +0300

    Update breast tumor seg.py

--- breast tumor seg.py
+++ breast tumor seg.py
@@ -2,10 +2,10 @@ import prediction.predict_rescaled as predictor
 import numpy as np
 import visualization.visiualize_2d.visualize_breast_tumor as visualize
 
-rescaled_array = np.load('/home/zhoul0a/Desktop/Breast_Cancer_MICCAI/new/rescaled_array_hayida/1724535_unknown.npz')['array']
+rescaled_array = np.load('/DCE-MRI_data/1724535_unknown.npz')['array']
 input_array = rescaled_array[:, :, :, 0: 3]
 gt_array = rescaled_array[:, :, :, 3]
-predict_array = predictor.predict_breast_tumor_dcm_mri(input_array, check_point_top_dict='/home/zhoul0a/Desktop/outside_share/trained_models/')
+predict_array = predictor.predict_breast_tumor_dcm_mri(input_array, check_point_top_dict='/trained_models/')
 visualize.analysis_prediction(input_array, predict_array, gt_array)
 
 
